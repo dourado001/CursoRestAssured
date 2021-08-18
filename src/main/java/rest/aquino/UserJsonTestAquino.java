318c2b97bfa4d118c355f53e00bd4b618ce0ed5d
Guilherme Barbosa <guilherme.dourado95@gmail.com>
2021-08-18 18:45:34 -0300

    Atributos estáticos

--- src/main/java/rest/aquino/UserJsonTestAquino.java
+++ src/main/java/rest/aquino/UserJsonTestAquino.java
@@ -1,8 +1,14 @@
 package rest.aquino;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.Method;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,13 +16,33 @@ import static org.hamcrest.Matchers.*;
 
 public class UserJsonTestAquino {
 
+    public static RequestSpecification reqSpec;
+    public static ResponseSpecification resSpec;
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = "http://restapi.wcaquino.me";
+        RestAssured.port = 80;
+        //RestAssured.basePath = "/v2";
+
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.log(LogDetail.ALL);
+        RestAssured.requestSpecification = reqBuilder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectStatusCode(200);
+        RestAssured.responseSpecification = resBuilder.build();
+
+//        RestAssured.responseSpecification = resSpec;
+//        RestAssured.requestSpecification = reqSpec;
+    }
+
     @Test
     public void deveTrazerJsonPrimeiroNivel(){
                 given()
                 .when()
-                        .get("http://restapi.wcaquino.me/users/3")
+                        .get("/users/3")
                 .then()
-                        .statusCode(200)
                         .body("id", is(3))
                         .body("name",is("Ana Júlia"))
                         .body("age",is(20))
@@ -28,9 +54,8 @@ public class UserJsonTestAquino {
 
                 given()
                 .when()
-                        .get("http://restapi.wcaquino.me/users/2")
+                        .get("/users/2")
                 .then()
-                        .statusCode(200)
                         .body("endereco.numero",is(0))
                 ;
     }
@@ -39,9 +64,8 @@ public class UserJsonTestAquino {
 
         given()
                 .when()
-                    .get("http://restapi.wcaquino.me/users/3")
+                    .get("/users/3")
                 .then()
-                .statusCode(200)
                     .body("filhos",hasSize(2))
                     .body("filhos[0].name",is("Zezinho"))
                     .body("filhos[1].name",is("Luizinho"))
