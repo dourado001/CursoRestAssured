2279e495bcdad9fe52ed05ae162e1a613f23b989
Guilherme Barbosa <guilherme.dourado95@gmail.com>
2021-08-24 22:12:05 -0300

    Finalizando secao de Verbos

--- src/main/java/rest/aquino/VerbosTest.java
+++ src/main/java/rest/aquino/VerbosTest.java
@@ -1,7 +1,7 @@
 package rest.aquino;
 
 import io.restassured.RestAssured;
-import org.hamcrest.Matchers;
+import io.restassured.http.ContentType;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -19,7 +19,7 @@ public class VerbosTest {
     public void deveSalvarUsuarios(){
                 given()
                         .log().all()
-                        .contentType("application/json")
+                        .contentType(ContentType.JSON)
                         .body("{ \"name\" : \"Guilherme\", \"age\": 26}")
                 .when()
                         .post("/users")
@@ -32,34 +32,11 @@ public class VerbosTest {
                 ;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Test
     public void naoDeveSalvarUsuario(){
                 given()
                         .log().all()
-                        .contentType("application/json")
+                        .contentType(ContentType.JSON)
                         .body("{\"age\": 69}")
                 .when()
                         .post("/users")
@@ -69,5 +46,71 @@ public class VerbosTest {
                         .body("error",is("Name é um atributo obrigatório"))
                 ;
     }
-}
 
+    @Test
+    public void deveAlterarUsuario(){
+                given()
+                        .log().all()
+                        .contentType(ContentType.JSON)
+                        .body("{\"name\": \"Creuza\",\"endereco\": {\"rua\": \"Rua dos troxa\",\"numero\": 69},\"age\": \"99\",\"salary\": \"10000f\"}")
+                .when()
+                        .put("users/2")
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name",is("Creuza"))
+                        .body("age",is("99"))
+                        .body("endereco.rua",is("Rua dos troxa"))
+                        .body("endereco.numero",is(69))
+                        .body("salary",is("10000f"))
+                ;
+    }
+
+    @Test
+    public void deveCustomizarURL(){
+                given()
+                        .log().all()
+                        .contentType(ContentType.JSON)
+                        .body("{ \"name\" : \"Goku\", \"age\": 201}")
+                        .pathParams("entidade","users")
+                        .pathParams("id","1")
+                .when()
+                        .put("/{entidade}/{id}")
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name",is("Goku"))
+                        .body("age",is(201))
+                ;
+    }
+
+    @Test
+    public void deveRemoverUsuario(){
+                given()
+                        .log().all()
+                        .pathParams("entidade","users")
+                        .pathParams("id","3")
+                .when()
+                        .delete("/{entidade}/{id}")
+                .then()
+                        .log().all()
+                        .statusCode(204)
+                ;
+    }
+
+    @Test
+    public void naoDeveDeletarUsuarioInexistente(){
+                given()
+                        .log().all()
+                        .contentType(ContentType.JSON)
+                        .pathParams("entidade","users")
+                        .pathParams("id",99)
+                .when()
+                        .delete("/{entidade}/{id}")
+                .then()
+                        .log().all()
+                        .statusCode(400)
+                        .body("error",is("Registro inexistente"))
+                ;
+    }
+}
